4c414c78e4d670eabc934678bfd5ec5321d65f62
CryFecteau <Crystal_Fecteau@ymail.com>
2020-03-06 12:47:23 -0800

    adjust animations add in modals and import custom images

--- main.js
+++ main.js
@@ -1,12 +1,44 @@
+//navbar
 const selectElement = function (element) {
     return document.querySelector(element);
 };
 let menuToggler = selectElement('.menu-toggle');
 let body = selectElement('body');
+let nav = selectElement('.nav-list');
 
 menuToggler.addEventListener('click', function () {
-    body.classList.toggle('open')
+    body.classList.toggle('open');
 });
+nav.onclick = function () {
+    body.classList.remove('open');
+}
+
+// modals
+let customModal = document.getElementById('customsModal');
+let customBtn = document.getElementById('customBtn');
+let recipeModal = document.getElementById('recipesModal');
+let recipeBtn = document.getElementById('recipesBtn');
+let spanCustom = document.getElementById('closeCustoms');
+let spanRecipe = document.getElementById('closeRecipes')
+
+customBtn.onclick = function () {
+    customModal.style.display = 'block';
+}
+recipeBtn.onclick = function () {
+    recipeModal.style.display = 'block';
+}
+spanCustom.onclick = function () {
+    customModal.style.display = 'none';
+}
+spanRecipe.onclick = function () {
+    recipeModal.style.display = 'none';
+}
+window.onclick = function (event) {
+    if (event.target == customModal || event.target == recipeModal) {
+        customModal.style.display = 'none';
+        recipeModal.style.display = 'none';
+    }
+}
 
 // scroll reveal
 window.sr = ScrollReveal();
